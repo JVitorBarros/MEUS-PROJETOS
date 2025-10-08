92fad5d1bee21b7a13941fac6585cc08651e4e44
João Vitor Da Silva Barros <jvitorbarros84@gmail.com>
2025-10-08 17:23:18 -0300

    ad

--- 1 - Lista de extensões/javascript.js
+++
@@ -1,5 +0,0 @@
-const temas = [...document.querySelectorAll('.caixa-temas')];
-const temaClaro = document.querySelector('#tema-claro');
-const temaEscuro = document.querySelector('#tema-escuro');
-
-temas.AddEventListener('click', alert('clcicou'));
