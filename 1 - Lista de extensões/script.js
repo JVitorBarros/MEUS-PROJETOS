40f866b2373bf9775941bd7483381e94950908ce
João Vitor Da Silva Barros <jvitorbarros84@gmail.com>
2025-10-12 20:47:41 -0300

    add

---
+++ 1 - Lista de extensões/script.js
@@ -0,0 +1,24 @@
+const body = document.body;
+const botaoTema = document.querySelector('.tema');
+const modoEscuro = document.querySelector('#modo-escuro');
+const modoClaro = document.querySelector('#modo-claro');
+
+let temaAtual = 'escuro';
+
+function alterarTema(){
+    if(temaAtual === 'escuro'){
+        body.classList.replace('tema-escuro', 'tema-claro');
+        modoEscuro.classList.remove('ativo');
+        modoClaro.classList.add('ativo');
+
+        temaAtual = 'claro';
+    }else{
+        body.classList.replace('tema-claro','tema-escuro');
+        modoClaro.classList.remove('ativo');
+        modoEscuro.classList.add('ativo');
+
+        temaAtual = 'escuro';
+    }
+}
+
+botaoTema.addEventListener('click', alterarTema);
